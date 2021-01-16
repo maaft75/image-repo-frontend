795f0f68bbfb8aee431ca4c829a108e3b8fac38c
maaft75 <ayodejiadedeji8@gmail.com>
2021-01-16 16:11:59 +0100

    updatedProdDeets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  frontBaseUrl : "http://localhost:4200/",
+  baseUrl : "https://localhost:44361/api/",
+  firebaseConfig : {
+    apiKey: "AIzaSyD1iaa_9vpSAAdGspHVJaCPDRWbUZFnYKc",
+    authDomain: "image-repo-d5486.firebaseapp.com",
+    projectId: "image-repo-d5486",
+    storageBucket: "image-repo-d5486.appspot.com",
+    messagingSenderId: "932900508554",
+    appId: "1:932900508554:web:e4eb35a1362a9b33786e73",
+    measurementId: "G-56HZ7WBJN5"
+  }
 };
